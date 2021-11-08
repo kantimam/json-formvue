9e6eb52eab6ac3f08b765e60ee0562fc2094092e
Lukas Leicher <l.leicher@ondigo.de>
2021-11-08 11:34:51 +0100

    [TASK] refresh captcha image implemented

--- src/components/fields/onCaptcha.vue
+++ src/components/fields/onCaptcha.vue
@@ -2,14 +2,32 @@
 
 <template>
   <div v-if="properties && properties.gencaptchaUri" class="ondigo-captcha-container">
-    <div class="ondigo-captcha-img-container">
-      <img :src="properties.gencaptchaUri" >
+    <div class="ondigo-captcha-img-container" ref="captchaImgContainer" />
+    <div class="ondigo-captcha-refresh-container">
+      <a 
+        v-if="!loading"
+        href="#"
+        class="ondigo-captcha-refresh-text"
+        @click="refresh">
+        {{ properties.refreshText }}
+      </a>
+      <v-progress-circular
+        v-else
+        indeterminate
+        size="20"
+        width="3"
+        color="primary" />
+    </div>
+    <div v-if="error" class="ondigo-captch-error">
+      <p>
+        {{ error }}
+      </p>
     </div>
     <text-field v-bind="{...$props, ...$attrs}" v-on="$listeners" type="text" />
   </div>
   <div v-else class="ondigo-captch-error">
     <p>
-      could not load  Captcha try refreshing the page
+      could not load Captcha try refreshing the page
     </p>
   </div>
 </template>
@@ -20,6 +38,47 @@ export default {
   name: "onCaptcha",
   components: {
     TextField
+  },
+  data: () => ({
+    loading: false,
+    error: null,
+  }),
+  created: function() {
+    this.preloadImage()
+      .then(img => this.setImg(img))
+      .catch(() => this.error = 'Failed to fetch image.');
+  },
+  methods: {
+    refresh(event) {
+      event.preventDefault();
+      if (this.loading) return;
+
+      this.loading = true;
+      this.preloadImage()
+        .then(img => {
+          this.setImg(img);
+          this.loading = false;
+        })
+        .catch(() => {
+          this.error = 'Failed to fetch image.';
+          this.loading = false;
+        });
+    },
+    preloadImage() {
+      return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.addEventListener('load', () => resolve(image));
+        image.addEventListener('error', err => reject(err));
+
+        image.src = this.properties.gencaptchaUri;
+      });  
+    },
+    setImg(img) {
+      const container = this.$refs.captchaImgContainer;
+      while (container.lastElementChild) container.removeChild(container.lastElementChild);
+
+      container.appendChild(img);
+    },
   },
   props: {
     properties: {
