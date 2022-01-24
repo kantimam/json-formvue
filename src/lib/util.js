a1ff30c3279cbc71ac9cfdaaf95b2162d55fa142
Lukas Leicher <l.leicher@ondigo.de>
2022-01-24 16:22:30 +0100

    [TASK] added note to email pattern

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,5 +1,5 @@
-import { getMaskPatternMapping, matchMaskPattern } from "./pattern";
-import { compareDateTimes, currentIsoTime, parseISODateFromPattern } from "./time";
+import {getMaskPatternMapping, matchMaskPattern} from "./pattern";
+import {compareDateTimes, currentIsoTime, parseISODateFromPattern} from "./time";
 
 export const createInputName = (formName, inputName) => `tx_form_formframework[${formName}][${inputName}]`;
 
@@ -104,6 +104,8 @@ export const validatorLength = (string, invalidMessage, vArgs) => {
 };
 export const validatorAlphanumeric = (string, invalidMessage) => /^[a-z0-9]+$/i.test(string) || invalidMessage
 export const validatorEmail = (string, invalidMessage) => {
+    // Modified Regex which ensures at least 2 TLD chars
+    // const emailRegex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]{2,}|\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])$/
     const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     return emailRegex.test(string) || invalidMessage
 }
