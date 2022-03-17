7109ab81998baaa7a07e18802603b810eed81303
k.imamov <k.imamov@ondigo.de>
2022-03-17 10:05:03 +0100

    init storybook

--- src/stories/TextField.stories.js
+++ src/stories/TextField.stories.js
@@ -1,14 +1,20 @@
 
-import InternalTextField from "../components/fields/extended_text_field";
+import TextField from "../components/fields/textfield.vue";
 import Vue from "vue";
 import {VTextField} from "vuetify/lib/components";
+import Vuetify from 'vuetify/lib/framework';
+
+
+Vue.use(Vuetify);
+const vuetify=new Vuetify({})
+
 
 Vue.component('v-text-field', VTextField)
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
     title: 'Formvue/TextField',
-    component: InternalTextField,
+    component: TextField,
     // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
     argTypes: {
 
@@ -18,8 +24,9 @@ export default {
 // More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template = (args, { argTypes }) => ({
     props: Object.keys(argTypes),
-    components: { InternalTextField },
-    template: '<internal-text-field v-bind="$props" />',
+    vuetify: vuetify,
+    components: { TextField },
+    template: '<text-field v-bind="$props" />',
 });
 
 export const Default = Template.bind({});
@@ -30,8 +37,15 @@ Default.args = {
     properties: {},
     name: 'test',
 
-
 };
 
+export const WithName = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+WithName.args = {
+    label: 'With Name',
+    id: 'test',
+    properties: {},
+    name: 'test',
 
+};
 
