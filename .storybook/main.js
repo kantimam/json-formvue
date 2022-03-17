3351a5dc82d7b206e6d8614b48068ebd8fda0c6a
k.imamov <k.imamov@ondigo.de>
2022-03-17 10:08:47 +0100

    init storybook

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+//const path = require('path') // used for resolving aliases
+const custom=require('@vue/cli-service/webpack.config.js');
+
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/vue",
+    webpackFinal: async (config) => {
+      return { ...config, module: { ...config.module, rules: custom.module.rules } };
+    },
+
+
+}
